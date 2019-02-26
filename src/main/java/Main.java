ed1c46dc563dc277a4802d12305fd10bc5f068f1
Олег Сахарук <saharukoleg1@gmail.com>
2019-02-26 20:46:39 +0200

    fix task

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,14 @@
+
+import lombok.Getter;
+import lombok.Setter;
+
 public class Main {
+    @Getter
+    @Setter
+    String str = "Hello";
+
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println(new Main().getStr());
+
     }
 }
